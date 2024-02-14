15def45316bd3b322f959b51ee65230d3c9fd37d
GustavoCoelho295 <iago.pinheirodeoliveira@gmail.com>
2024-02-14 08:27:21 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import productsController from "./controllers/products";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3333;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
